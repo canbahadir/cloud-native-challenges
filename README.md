dcfc3f4eb9904ac6da86843db985b450f3843802
Bahadır Can <imbahadircan@gmail.com>
2021-05-10 12:39:38 +0000

    Added task 3 of first challenge.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # cloud-native-challenges
 Challenges I worked on from various sources.
+
+
+## BCFM
+
+### TASK 0
+
+### TASK 1
+
+### TASK 2
+
+### TASK 3
+Create an app which serves "/bcfm" page. 
+When a request sent it should have your name.
+
+STEPS:
+
+    cd task3/dockerapp
+    docker build -t node-app:0.1 .
+    docker images
+    sudo docker run -p 4000:80 --name my-app node-app:0.1
+    curl http://localhost:4000/bcfm
+
+### TASK 4
+
+### TASK 5
+
+### TASK 6
---
+++ task3/dockerapp/app.js
@@ -0,0 +1,26 @@
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 80;
+
+const server = http.createServer((req, res) => {
+    if (req.url == '/bcfm'){
+        res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+                res.end('<h1>Bahadir Can</h1>');
+
+    } else {
+        res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+                res.end('<h1>Hello World</h1>');
+    }
+});
+
+server.listen(port, hostname, () => {
+    console.log('Server running at http://%s:%s/', hostname, port);
+});
+
+process.on('SIGINT', function() {
+    console.log('Caught interrupt signal and will exit');
+    process.exit();
+});

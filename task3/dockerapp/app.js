f354f87c64c37150d5108de4577a8e5ad0d75f92
Bahadır Can <imbahadircan@gmail.com>
2021-06-08 05:46:25 +0000

    Fixed app.js.

--- task3/dockerapp/app.js
+++ task3/dockerapp/app.js
@@ -12,7 +12,7 @@ const server = http.createServer((req, res) => {
     } else {
         res.statusCode = 200;
             res.setHeader('Content-Type', 'text/html');
-            res.end('<h1>Hello World</h1>');
+                res.end('<h1>Hello World</h1>');
     }
 });
 
